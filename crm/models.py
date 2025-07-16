29c3382677835b15ba2267eca6eb5c0c077a2211
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2025-07-16 02:02:36 +0100

    feat: add string representation methods for Customer, Product, and Order models

--- crm/models.py
+++ crm/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# id's ar implicitly added to the db
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     name =models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -20,3 +26,5 @@ class Order(models.Model):
     order_date = models.DateTimeField(default=timezone.now)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
